6ba24d9837b640743ee5db4b2548a28791cd3a96
fantasyzernontra <fantasyzernontra@hotmail.com>
2020-08-04 22:29:58 +0700

    [FIX]

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
 const StandardAdultCoffinRoute = require('./src/routers/standardAdultCoffin')
 const StandardChildCoffinRoute = require('./src/routers/standardChildCoffin')
 const ModernAdultCoffinRoute = require('./src/routers/modernAdultCoffin')
 const ModernChildCoffinRoute = require('./src/routers/modernChildCoffin')
-
-app.use(cors({
-    origin: ['http://localhost:3000'],
-    credentials: true
-}));
 
 app.use(express.json())
 
